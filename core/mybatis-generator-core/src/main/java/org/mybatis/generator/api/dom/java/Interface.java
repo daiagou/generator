66499a455dfdb4404abaea995826790ccf5fe37c
abner.zhang <abnerzhang@kargocard.com>
2018-04-17 09:48:50 +0800

    import List

--- core/mybatis-generator-core/src/main/java/org/mybatis/generator/api/dom/java/Interface.java
+++ core/mybatis-generator-core/src/main/java/org/mybatis/generator/api/dom/java/Interface.java
@@ -1,5 +1,5 @@
 /**
- *    Copyright 2006-2017 the original author or authors.
+ *    Copyright 2006-2018 the original author or authors.
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
@@ -102,6 +102,8 @@ public class Interface extends InnerInterface implements CompilationUnit {
         }
 
 
+        sb.append("import java.util.List;");
+        newLine(sb);
         sb.append("import org.apache.ibatis.annotations.Param;");
         newLine(sb);
         sb.append("import org.springframework.data.domain.Pageable;");
